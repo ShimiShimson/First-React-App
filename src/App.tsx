97d09df9581b9f601e08d38ce466bfd4f0acbb42
ShimiShimson <szymon.jakub.adamczuk@gmail.com>
2021-03-23 15:08:26 +0100

    extract markup into components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function UserInfo(props: any) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  )
+}
+
+function Avatar(props: any) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+function Comment(props: any) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {props.date.toLocaleDateString()}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
+
 function Welcome(props: any) {
   return <h1>Hello, {props.name}</h1>;
 }
@@ -11,9 +54,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -22,6 +62,11 @@ function App() {
         >
           Learn React
         </a>
+        <Comment
+          date={comment.date}
+          text={comment.text}
+          author={comment.author}
+        />
         <Welcome name="Bartek" />
         <Welcome name="Szymon" />
         <Welcome name="Dawid" />
